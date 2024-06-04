64b1c426f0060d648d9432a8cc1dfe9a9fe597fe
Yassine Mabsoute <ymabsout@e2r4p12.1337.ma>
2024-06-04 18:15:52 +0100

    finish this later

--- cpp42/cpp00/ex02/Account.cpp
+++ cpp42/cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: ymabsout <ymabsout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 15:01:28 by ymabsout          #+#    #+#             */
-/*   Updated: 2024/06/04 17:49:24 by ymabsout         ###   ########.fr       */
+/*   Updated: 2024/06/04 18:15:39 by ymabsout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int Account::_totalNbWithdrawals = 0;
 void Account::_displayTimestamp(void){
     std::time_t kk = std::time(0);
     std::tm *ref = std::localtime(&kk);
-    std::cout << "[" << ref->tm_year << ref->tm_mon << ref->
+    std::cout << "[" << ref->tm_year << ref->tm_mon << ref->tm_mday 
+        << "_" << ref->tm_hour << ref->tm_min << ref->tm_sec << "] ";
 
 }
 
@@ -31,6 +32,6 @@ Account::Account(int initial_deposit) {
     _nbDeposits = 0;
     _nbWithdrawals = 0;
     _amount = initial_deposit;
-    
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created"<< std::endl;
 }
 
