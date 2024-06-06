33788003ace2b7bd0ea2d0162ff412d2ea3493ff
Yassine Mabsoute <ymabsout@e2r2p11.1337.ma>
2024-06-06 20:47:41 +0100

    finish later

---
+++ cpp42/cpp01/ex04/Header.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymabsout <ymabsout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 20:12:48 by ymabsout          #+#    #+#             */
+/*   Updated: 2024/06/06 20:47:31 by ymabsout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+class Replacer{
+    private:
+        std::string s2;
+        std::string s1;
+    public:
+    Replacer()
+}
