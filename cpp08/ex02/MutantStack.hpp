71240f02ba7232013c74772eca1e1c578d213ab0
Yassine Mabsoute <ymabsout@e3r1p7.1337.ma>
2024-07-10 21:45:54 +0100

    alias commit

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,11 +6,29 @@
 #define MUTANTSTACK_HPP
 
 template < typename T > 
-class MutantStack : public std::stack <T> {
+class MutantStack : public std::stack <T>  {
     public:
-    MutantStack <T> ();
-    MutantStack(const MutantStack &);
+    MutantStack () : std::stack<T>(){
+        std::cout << "Mutant stack constructor called" << std::endl;
+    };
 
+    MutantStack(const MutantStack &objs) : std::stack <T> (objs){
+        std::cout << "Mutant copy constructor called" << std::endl;
+    };
+
+    MutantStack &operator=(const MutantStack & objs){
+        if (this != &objs){
+            std::cout << "Mutant copy assignement operator called" << std::endl;
+            std::stack<T>::operator=(objs);
+        }
+        return (*this);
+    }
+
+    ~MutantStack(){
+        std::cout << "Mutant destructor called " << std::endl;
+    }
+
+    
 };
 
 
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -3,5 +3,15 @@
 int main (){
     MutantStack<int> mstack;
     mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    std::cout << mstack.size() << std::endl;
+    MutantStack < int > a ;
+    a = mstack;
+    a.push(20);
+    std::cout << a.top() << std::endl;
+    a.pop();
+    a.pop();
+    std::cout << a.top() << std::endl;
 }
 
